b125b921752a0392a33855112e067d5f63bc906f
san2304 <khoirulikhsan66@gmail.com>
2023-06-24 12:48:17 +0700

    first commit

---
+++ algo_latihan5.py
@@ -0,0 +1,11 @@
+tuple_1 = ("Faqih","Nicko","Ageng","Ikhsan","Adin")
+tuple_2 = ("23","42","12","53","64")
+tuple_3 = ("Puri","Putri","Syifa","Anggun")
+
+print(tuple_1)
+print(tuple_2)
+print(tuple_4)
+
+#membuat tuple yang berhasil kosong
+empty_tuple = ()
+print("tuple ksong", empty_tuple)
